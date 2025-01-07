453ce5a3f27d6eecf5aa1d4ff7b64b1f99320949
R <lariverosc@gmail.com>
2025-01-07 11:01:53 -0500

    Create countries_sp.sql

---
+++ sql/countries_sp.sql
@@ -0,0 +1,252 @@
+ALTER TABLE countries ADD COLUMN name_sp varchar(100);
+
+UPDATE countries SET name_sp = 'Afganistán' WHERE id = 1; -- Afghanistan
+UPDATE countries SET name_sp = 'Islas Åland' WHERE id = 2; -- Aland Islands
+UPDATE countries SET name_sp = 'Albania' WHERE id = 3; -- Albania
+UPDATE countries SET name_sp = 'Argelia' WHERE id = 4; -- Algeria
+UPDATE countries SET name_sp = 'Samoa Americana' WHERE id = 5; -- American Samoa
+UPDATE countries SET name_sp = 'Andorra' WHERE id = 6; -- Andorra
+UPDATE countries SET name_sp = 'Angola' WHERE id = 7; -- Angola
+UPDATE countries SET name_sp = 'Anguila' WHERE id = 8; -- Anguilla
+UPDATE countries SET name_sp = 'Antártida' WHERE id = 9; -- Antarctica
+UPDATE countries SET name_sp = 'Antigua y Barbuda' WHERE id = 10; -- Antigua and Barbuda
+UPDATE countries SET name_sp = 'Argentina' WHERE id = 11; -- Argentina
+UPDATE countries SET name_sp = 'Armenia' WHERE id = 12; -- Armenia
+UPDATE countries SET name_sp = 'Aruba' WHERE id = 13; -- Aruba
+UPDATE countries SET name_sp = 'Australia' WHERE id = 14; -- Australia
+UPDATE countries SET name_sp = 'Austria' WHERE id = 15; -- Austria
+UPDATE countries SET name_sp = 'Azerbaiyán' WHERE id = 16; -- Azerbaijan
+UPDATE countries SET name_sp = 'Bahrein' WHERE id = 18; -- Bahrain
+UPDATE countries SET name_sp = 'Bangladés' WHERE id = 19; -- Bangladesh
+UPDATE countries SET name_sp = 'Barbados' WHERE id = 20; -- Barbados
+UPDATE countries SET name_sp = 'Bielorrusia' WHERE id = 21; -- Belarus
+UPDATE countries SET name_sp = 'Bélgica' WHERE id = 22; -- Belgium
+UPDATE countries SET name_sp = 'Belice' WHERE id = 23; -- Belize
+UPDATE countries SET name_sp = 'Benín' WHERE id = 24; -- Benin
+UPDATE countries SET name_sp = 'Bermudas' WHERE id = 25; -- Bermuda
+UPDATE countries SET name_sp = 'Bután' WHERE id = 26; -- Bhutan
+UPDATE countries SET name_sp = 'Bolivia' WHERE id = 27; -- Bolivia
+UPDATE countries SET name_sp = 'Bonaire, San Eustaquio y Saba' WHERE id = 155; -- Bonaire, Sint Eustatius and Saba
+UPDATE countries SET name_sp = 'Bosnia y Herzegovina' WHERE id = 28; -- Bosnia and Herzegovina
+UPDATE countries SET name_sp = 'Botsuana' WHERE id = 29; -- Botswana
+UPDATE countries SET name_sp = 'Isla Bouvet' WHERE id = 30; -- Bouvet Island
+UPDATE countries SET name_sp = 'Brasil' WHERE id = 31; -- Brazil
+UPDATE countries SET name_sp = 'Territorio Británico del Océano Índico' WHERE id = 32; -- British Indian Ocean Territory
+UPDATE countries SET name_sp = 'Brunéi' WHERE id = 33; -- Brunei
+UPDATE countries SET name_sp = 'Bulgaria' WHERE id = 34; -- Bulgaria
+UPDATE countries SET name_sp = 'Burkina Faso' WHERE id = 35; -- Burkina Faso
+UPDATE countries SET name_sp = 'Burundi' WHERE id = 36; -- Burundi
+UPDATE countries SET name_sp = 'Camboya' WHERE id = 37; -- Cambodia
+UPDATE countries SET name_sp = 'Camerún' WHERE id = 38; -- Cameroon
+UPDATE countries SET name_sp = 'Canadá' WHERE id = 39; -- Canada
+UPDATE countries SET name_sp = 'Cabo Verde' WHERE id = 40; -- Cape Verde
+UPDATE countries SET name_sp = 'Islas Caimán' WHERE id = 41; -- Cayman Islands
+UPDATE countries SET name_sp = 'República Centroafricana' WHERE id = 42; -- Central African Republic
+UPDATE countries SET name_sp = 'Chad' WHERE id = 43; -- Chad
+UPDATE countries SET name_sp = 'Chile' WHERE id = 44; -- Chile
+UPDATE countries SET name_sp = 'China' WHERE id = 45; -- China
+UPDATE countries SET name_sp = 'Isla de Navidad' WHERE id = 46; -- Christmas Island
+UPDATE countries SET name_sp = 'Islas Cocos (Keeling)' WHERE id = 47; -- Cocos (Keeling) Islands
+UPDATE countries SET name_sp = 'Colombia' WHERE id = 48; -- Colombia
+UPDATE countries SET name_sp = 'Comoras' WHERE id = 49; -- Comoros
+UPDATE countries SET name_sp = 'Congo' WHERE id = 50; -- Congo
+UPDATE countries SET name_sp = 'Islas Cook' WHERE id = 52; -- Cook Islands
+UPDATE countries SET name_sp = 'Costa Rica' WHERE id = 53; -- Costa Rica
+UPDATE countries SET name_sp = 'Costa de Marfil' WHERE id = 54; -- Cote D'Ivoire (Ivory Coast)
+UPDATE countries SET name_sp = 'Croacia' WHERE id = 55; -- Croatia
+UPDATE countries SET name_sp = 'Cuba' WHERE id = 56; -- Cuba
+UPDATE countries SET name_sp = 'Curaçao' WHERE id = 249; -- Curaçao
+UPDATE countries SET name_sp = 'Chipre' WHERE id = 57; -- Cyprus
+UPDATE countries SET name_sp = 'República Checa' WHERE id = 58; -- Czech Republic
+UPDATE countries SET name_sp = 'República Democrática del Congo' WHERE id = 51; -- Democratic Republic of the Congo
+UPDATE countries SET name_sp = 'Dinamarca' WHERE id = 59; -- Denmark
+UPDATE countries SET name_sp = 'Yibuti' WHERE id = 60; -- Djibouti
+UPDATE countries SET name_sp = 'Dominica' WHERE id = 61; -- Dominica
+UPDATE countries SET name_sp = 'República Dominicana' WHERE id = 62; -- Dominican Republic
+UPDATE countries SET name_sp = 'Ecuador' WHERE id = 64; -- Ecuador
+UPDATE countries SET name_sp = 'Egipto' WHERE id = 65; -- Egypt
+UPDATE countries SET name_sp = 'El Salvador' WHERE id = 66; -- El Salvador
+UPDATE countries SET name_sp = 'Guinea Ecuatorial' WHERE id = 67; -- Equatorial Guinea
+UPDATE countries SET name_sp = 'Eritrea' WHERE id = 68; -- Eritrea
+UPDATE countries SET name_sp = 'Estonia' WHERE id = 69; -- Estonia
+UPDATE countries SET name_sp = 'Eswatini' WHERE id = 212; -- Eswatini
+UPDATE countries SET name_sp = 'Etiopía' WHERE id = 70; -- Ethiopia
+UPDATE countries SET name_sp = 'Islas Malvinas' WHERE id = 71; -- Falkland Islands
+UPDATE countries SET name_sp = 'Islas Feroe' WHERE id = 72; -- Faroe Islands
+UPDATE countries SET name_sp = 'Fiyi' WHERE id = 73; -- Fiji Islands
+UPDATE countries SET name_sp = 'Finlandia' WHERE id = 74; -- Finland
+UPDATE countries SET name_sp = 'Francia' WHERE id = 75; -- France
+UPDATE countries SET name_sp = 'Guayana Francesa' WHERE id = 76; -- French Guiana
+UPDATE countries SET name_sp = 'Polinesia Francesa' WHERE id = 77; -- French Polynesia
+UPDATE countries SET name_sp = 'Territorios Australes Franceses' WHERE id = 78; -- French Southern Territories
+UPDATE countries SET name_sp = 'Gabón' WHERE id = 79; -- Gabon
+UPDATE countries SET name_sp = 'Gambia' WHERE id = 80; -- Gambia The
+UPDATE countries SET name_sp = 'Georgia' WHERE id = 81; -- Georgia
+UPDATE countries SET name_sp = 'Alemania' WHERE id = 82; -- Germany
+UPDATE countries SET name_sp = 'Ghana' WHERE id = 83; -- Ghana
+UPDATE countries SET name_sp = 'Gibraltar' WHERE id = 84; -- Gibraltar
+UPDATE countries SET name_sp = 'Grecia' WHERE id = 85; -- Greece
+UPDATE countries SET name_sp = 'Groenlandia' WHERE id = 86; -- Greenland
+UPDATE countries SET name_sp = 'Granada' WHERE id = 87; -- Grenada
+UPDATE countries SET name_sp = 'Guadalupe' WHERE id = 88; -- Guadeloupe
+UPDATE countries SET name_sp = 'Guam' WHERE id = 89; -- Guam
+UPDATE countries SET name_sp = 'Guatemala' WHERE id = 90; -- Guatemala
+UPDATE countries SET name_sp = 'Guernsey y Alderney' WHERE id = 91; -- Guernsey and Alderney
+UPDATE countries SET name_sp = 'Guinea' WHERE id = 92; -- Guinea
+UPDATE countries SET name_sp = 'Guinea-Bisáu' WHERE id = 93; -- Guinea-Bissau
+UPDATE countries SET name_sp = 'Guyana' WHERE id = 94; -- Guyana
+UPDATE countries SET name_sp = 'Haití' WHERE id = 95; -- Haiti
+UPDATE countries SET name_sp = 'Islas Heard y McDonald' WHERE id = 96; -- Heard Island and McDonald Islands
+UPDATE countries SET name_sp = 'Honduras' WHERE id = 97; -- Honduras
+UPDATE countries SET name_sp = 'Hong Kong' WHERE id = 98; -- Hong Kong S.A.R.
+UPDATE countries SET name_sp = 'Hungría' WHERE id = 99; -- Hungary
+UPDATE countries SET name_sp = 'Islandia' WHERE id = 100; -- Iceland
+UPDATE countries SET name_sp = 'India' WHERE id = 101; -- India
+UPDATE countries SET name_sp = 'Indonesia' WHERE id = 102; -- Indonesia
+UPDATE countries SET name_sp = 'Irán' WHERE id = 103; -- Iran
+UPDATE countries SET name_sp = 'Irak' WHERE id = 104; -- Iraq
+UPDATE countries SET name_sp = 'Irlanda' WHERE id = 105; -- Ireland
+UPDATE countries SET name_sp = 'Israel' WHERE id = 106; -- Israel
+UPDATE countries SET name_sp = 'Italia' WHERE id = 107; -- Italy
+UPDATE countries SET name_sp = 'Jamaica' WHERE id = 108; -- Jamaica
+UPDATE countries SET name_sp = 'Japón' WHERE id = 109; -- Japan
+UPDATE countries SET name_sp = 'Jersey' WHERE id = 110; -- Jersey
+UPDATE countries SET name_sp = 'Jordania' WHERE id = 111; -- Jordan
+UPDATE countries SET name_sp = 'Kazajistán' WHERE id = 112; -- Kazakhstan
+UPDATE countries SET name_sp = 'Kenia' WHERE id = 113; -- Kenya
+UPDATE countries SET name_sp = 'Kiribati' WHERE id = 114; -- Kiribati
+UPDATE countries SET name_sp = 'Kosovo' WHERE id = 248; -- Kosovo
+UPDATE countries SET name_sp = 'Kuwait' WHERE id = 117; -- Kuwait
+UPDATE countries SET name_sp = 'Kirguistán' WHERE id = 118; -- Kyrgyzstan
+UPDATE countries SET name_sp = 'Laos' WHERE id = 119; -- Laos
+UPDATE countries SET name_sp = 'Letonia' WHERE id = 120; -- Latvia
+UPDATE countries SET name_sp = 'Líbano' WHERE id = 121; -- Lebanon
+UPDATE countries SET name_sp = 'Lesoto' WHERE id = 122; -- Lesotho
+UPDATE countries SET name_sp = 'Liberia' WHERE id = 123; -- Liberia
+UPDATE countries SET name_sp = 'Libia' WHERE id = 124; -- Libya
+UPDATE countries SET name_sp = 'Liechtenstein' WHERE id = 125; -- Liechtenstein
+UPDATE countries SET name_sp = 'Lituania' WHERE id = 126; -- Lithuania
+UPDATE countries SET name_sp = 'Luxemburgo' WHERE id = 127; -- Luxembourg
+UPDATE countries SET name_sp = 'Macao' WHERE id = 128; -- Macau S.A.R.
+UPDATE countries SET name_sp = 'Madagascar' WHERE id = 130; -- Madagascar
+UPDATE countries SET name_sp = 'Malaui' WHERE id = 131; -- Malawi
+UPDATE countries SET name_sp = 'Malasia' WHERE id = 132; -- Malaysia
+UPDATE countries SET name_sp = 'Maldivas' WHERE id = 133; -- Maldives
+UPDATE countries SET name_sp = 'Malí' WHERE id = 134; -- Mali
+UPDATE countries SET name_sp = 'Malta' WHERE id = 135; -- Malta
+UPDATE countries SET name_sp = 'Isla de Man' WHERE id = 136; -- Man (Isle of)
+UPDATE countries SET name_sp = 'Islas Marshall' WHERE id = 137; -- Marshall Islands
+UPDATE countries SET name_sp = 'Martinica' WHERE id = 138; -- Martinique
+UPDATE countries SET name_sp = 'Mauritania' WHERE id = 139; -- Mauritania
+UPDATE countries SET name_sp = 'Mauricio' WHERE id = 140; -- Mauritius
+UPDATE countries SET name_sp = 'Mayotte' WHERE id = 141; -- Mayotte
+UPDATE countries SET name_sp = 'México' WHERE id = 142; -- Mexico
+UPDATE countries SET name_sp = 'Micronesia' WHERE id = 143; -- Micronesia
+UPDATE countries SET name_sp = 'Moldavia' WHERE id = 144; -- Moldova
+UPDATE countries SET name_sp = 'Mónaco' WHERE id = 145; -- Monaco
+UPDATE countries SET name_sp = 'Mongolia' WHERE id = 146; -- Mongolia
+UPDATE countries SET name_sp = 'Montenegro' WHERE id = 147; -- Montenegro
+UPDATE countries SET name_sp = 'Montserrat' WHERE id = 148; -- Montserrat
+UPDATE countries SET name_sp = 'Marruecos' WHERE id = 149; -- Morocco
+UPDATE countries SET name_sp = 'Mozambique' WHERE id = 150; -- Mozambique
+UPDATE countries SET name_sp = 'Myanmar' WHERE id = 151; -- Myanmar
+UPDATE countries SET name_sp = 'Namibia' WHERE id = 152; -- Namibia
+UPDATE countries SET name_sp = 'Nauru' WHERE id = 153; -- Nauru
+UPDATE countries SET name_sp = 'Nepal' WHERE id = 154; -- Nepal
+UPDATE countries SET name_sp = 'Países Bajos' WHERE id = 156; -- Netherlands
+UPDATE countries SET name_sp = 'Nueva Caledonia' WHERE id = 157; -- New Caledonia
+UPDATE countries SET name_sp = 'Nueva Zelanda' WHERE id = 158; -- New Zealand
+UPDATE countries SET name_sp = 'Nicaragua' WHERE id = 159; -- Nicaragua
+UPDATE countries SET name_sp = 'Níger' WHERE id = 160; -- Niger
+UPDATE countries SET name_sp = 'Nigeria' WHERE id = 161; -- Nigeria
+UPDATE countries SET name_sp = 'Niue' WHERE id = 162; -- Niue
+UPDATE countries SET name_sp = 'Isla Norfolk' WHERE id = 163; -- Norfolk Island
+UPDATE countries SET name_sp = 'Corea del Norte' WHERE id = 115; -- North Korea
+UPDATE countries SET name_sp = 'Macedonia del Norte' WHERE id = 129; -- North Macedonia
+UPDATE countries SET name_sp = 'Islas Marianas del Norte' WHERE id = 164; -- Northern Mariana Islands
+UPDATE countries SET name_sp = 'Noruega' WHERE id = 165; -- Norway
+UPDATE countries SET name_sp = 'Omán' WHERE id = 166; -- Oman
+UPDATE countries SET name_sp = 'Pakistán' WHERE id = 167; -- Pakistan
+UPDATE countries SET name_sp = 'Palaos' WHERE id = 168; -- Palau
+UPDATE countries SET name_sp = 'Territorios Palestinos Ocupados' WHERE id = 169; -- Palestinian Territory Occupied
+UPDATE countries SET name_sp = 'Panamá' WHERE id = 170; -- Panama
+UPDATE countries SET name_sp = 'Papúa Nueva Guinea' WHERE id = 171; -- Papua New Guinea
+UPDATE countries SET name_sp = 'Paraguay' WHERE id = 172; -- Paraguay
+UPDATE countries SET name_sp = 'Perú' WHERE id = 173; -- Peru
+UPDATE countries SET name_sp = 'Filipinas' WHERE id = 174; -- Philippines
+UPDATE countries SET name_sp = 'Isla Pitcairn' WHERE id = 175; -- Pitcairn Island
+UPDATE countries SET name_sp = 'Polonia' WHERE id = 176; -- Poland
+UPDATE countries SET name_sp = 'Portugal' WHERE id = 177; -- Portugal
+UPDATE countries SET name_sp = 'Puerto Rico' WHERE id = 178; -- Puerto Rico
+UPDATE countries SET name_sp = 'Catar' WHERE id = 179; -- Qatar
+UPDATE countries SET name_sp = 'Reunión' WHERE id = 180; -- Reunion
+UPDATE countries SET name_sp = 'Rumania' WHERE id = 181; -- Romania
+UPDATE countries SET name_sp = 'Rusia' WHERE id = 182; -- Russia
+UPDATE countries SET name_sp = 'Ruanda' WHERE id = 183; -- Rwanda
+UPDATE countries SET name_sp = 'Santa Elena' WHERE id = 184; -- Saint Helena
+UPDATE countries SET name_sp = 'San Cristóbal y Nieves' WHERE id = 185; -- Saint Kitts and Nevis
+UPDATE countries SET name_sp = 'Santa Lucía' WHERE id = 186; -- Saint Lucia
+UPDATE countries SET name_sp = 'San Pedro y Miquelón' WHERE id = 187; -- Saint Pierre and Miquelon
+UPDATE countries SET name_sp = 'San Vicente y las Granadinas' WHERE id = 188; -- Saint Vincent and the Grenadines
+UPDATE countries SET name_sp = 'San Bartolomé' WHERE id = 189; -- Saint-Barthelemy
+UPDATE countries SET name_sp = 'San Martín (parte francesa)' WHERE id = 190; -- Saint-Martin (French part)
+UPDATE countries SET name_sp = 'Samoa' WHERE id = 191; -- Samoa
+UPDATE countries SET name_sp = 'San Marino' WHERE id = 192; -- San Marino
+UPDATE countries SET name_sp = 'Santo Tomé y Príncipe' WHERE id = 193; -- Sao Tome and Principe
+UPDATE countries SET name_sp = 'Arabia Saudita' WHERE id = 194; -- Saudi Arabia
+UPDATE countries SET name_sp = 'Senegal' WHERE id = 195; -- Senegal
+UPDATE countries SET name_sp = 'Serbia' WHERE id = 196; -- Serbia
+UPDATE countries SET name_sp = 'Seychelles' WHERE id = 197; -- Seychelles
+UPDATE countries SET name_sp = 'Sierra Leona' WHERE id = 198; -- Sierra Leone
+UPDATE countries SET name_sp = 'Singapur' WHERE id = 199; -- Singapore
+UPDATE countries SET name_sp = 'Sint Maarten (parte neerlandesa)' WHERE id = 250; -- Sint Maarten (Dutch part)
+UPDATE countries SET name_sp = 'Eslovaquia' WHERE id = 200; -- Slovakia
+UPDATE countries SET name_sp = 'Eslovenia' WHERE id = 201; -- Slovenia
+UPDATE countries SET name_sp = 'Islas Salomón' WHERE id = 202; -- Solomon Islands
+UPDATE countries SET name_sp = 'Somalia' WHERE id = 203; -- Somalia
+UPDATE countries SET name_sp = 'Sudáfrica' WHERE id = 204; -- South Africa
+UPDATE countries SET name_sp = 'Georgia del Sur' WHERE id = 205; -- South Georgia
+UPDATE countries SET name_sp = 'Corea del Sur' WHERE id = 116; -- South Korea
+UPDATE countries SET name_sp = 'Sudán del Sur' WHERE id = 206; -- South Sudan
+UPDATE countries SET name_sp = 'España' WHERE id = 207; -- Spain
+UPDATE countries SET name_sp = 'Sri Lanka' WHERE id = 208; -- Sri Lanka
+UPDATE countries SET name_sp = 'Sudán' WHERE id = 209; -- Sudan
+UPDATE countries SET name_sp = 'Surinam' WHERE id = 210; -- Suriname
+UPDATE countries SET name_sp = 'Svalbard y Jan Mayen' WHERE id = 211; -- Svalbard and Jan Mayen Islands
+UPDATE countries SET name_sp = 'Suecia' WHERE id = 213; -- Sweden
+UPDATE countries SET name_sp = 'Suiza' WHERE id = 214; -- Switzerland
+UPDATE countries SET name_sp = 'Siria' WHERE id = 215; -- Syria
+UPDATE countries SET name_sp = 'Taiwán' WHERE id = 216; -- Taiwan
+UPDATE countries SET name_sp = 'Tayikistán' WHERE id = 217; -- Tajikistan
+UPDATE countries SET name_sp = 'Tanzania' WHERE id = 218; -- Tanzania
+UPDATE countries SET name_sp = 'Tailandia' WHERE id = 219; -- Thailand
+UPDATE countries SET name_sp = 'Las Bahamas' WHERE id = 17; -- The Bahamas
+UPDATE countries SET name_sp = 'Timor Oriental' WHERE id = 63; -- Timor-Leste
+UPDATE countries SET name_sp = 'Togo' WHERE id = 220; -- Togo
+UPDATE countries SET name_sp = 'Tokelau' WHERE id = 221; -- Tokelau
+UPDATE countries SET name_sp = 'Tonga' WHERE id = 222; -- Tonga
+UPDATE countries SET name_sp = 'Trinidad y Tobago' WHERE id = 223; -- Trinidad and Tobago
+UPDATE countries SET name_sp = 'Túnez' WHERE id = 224; -- Tunisia
+UPDATE countries SET name_sp = 'Turquía' WHERE id = 225; -- Turkey
+UPDATE countries SET name_sp = 'Turkmenistán' WHERE id = 226; -- Turkmenistan
+UPDATE countries SET name_sp = 'Islas Turcas y Caicos' WHERE id = 227; -- Turks and Caicos Islands
+UPDATE countries SET name_sp = 'Tuvalu' WHERE id = 228; -- Tuvalu
+UPDATE countries SET name_sp = 'Uganda' WHERE id = 229; -- Uganda
+UPDATE countries SET name_sp = 'Ucrania' WHERE id = 230; -- Ukraine
+UPDATE countries SET name_sp = 'Emiratos Árabes Unidos' WHERE id = 231; -- United Arab Emirates
+UPDATE countries SET name_sp = 'Reino Unido' WHERE id = 232; -- United Kingdom
+UPDATE countries SET name_sp = 'Estados Unidos' WHERE id = 233; -- United States
+UPDATE countries SET name_sp = 'Islas Menores de Estados Unidos' WHERE id = 234; -- United States Minor Outlying Islands
+UPDATE countries SET name_sp = 'Uruguay' WHERE id = 235; -- Uruguay
+UPDATE countries SET name_sp = 'Uzbekistán' WHERE id = 236; -- Uzbekistan
+UPDATE countries SET name_sp = 'Vanuatu' WHERE id = 237; -- Vanuatu
+UPDATE countries SET name_sp = 'Ciudad del Vaticano' WHERE id = 238; -- Vatican City State (Holy See)
+UPDATE countries SET name_sp = 'Venezuela' WHERE id = 239; -- Venezuela
+UPDATE countries SET name_sp = 'Vietnam' WHERE id = 240; -- Vietnam
+UPDATE countries SET name_sp = 'Islas Vírgenes Británicas' WHERE id = 241; -- Virgin Islands (British)
+UPDATE countries SET name_sp = 'Islas Vírgenes de los Estados Unidos' WHERE id = 242; -- Virgin Islands (US)
+UPDATE countries SET name_sp = 'Wallis y Futuna' WHERE id = 243; -- Wallis and Futuna Islands
+UPDATE countries SET name_sp = 'Sahara Occidental' WHERE id = 244; -- Western Sahara
+UPDATE countries SET name_sp = 'Yemen' WHERE id = 245; -- Yemen
+UPDATE countries SET name_sp = 'Zambia' WHERE id = 246; -- Zambia
+UPDATE countries SET name_sp = 'Zimbabue' WHERE id = 247; -- Zimbabwe
